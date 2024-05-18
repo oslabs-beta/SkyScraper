a667c89e0350ab908720e194b531d8b758ce69be
Abel Ratanaphan <abelrat@yahoo.com>
2024-05-18 02:22:43 -0400

    Friday work

--- server/src/routers/AWSRouter.ts
+++ server/src/routers/AWSRouter.ts
@@ -3,12 +3,21 @@ import AWSController from '../controllers/AWSController.js';
 
 const AWSRouter = express.Router();
 
-AWSRouter.get('/ec2', AWSController.getEC2Instances, (req: Request, res: Response) => {
-  res.status(200).send(res.locals.instances);
-});
+AWSRouter.get(
+  '/ec2',
+  AWSController.getEC2Instances,
+  (req: Request, res: Response, next: NextFunction) => {
+    return res.status(200).send(res.locals.instances);
+  },
+);
 
-AWSRouter.get('/stats', AWSController.getEC2Instances, AWSController.getMetricStatistics, (req: Request, res: Response) => {
-  res.status(200).send(res.locals.metrics);
-});
+AWSRouter.get(
+  '/stats',
+  AWSController.getEC2Instances,
+  AWSController.getMetricStatistics,
+  (req: Request, res: Response, next: NextFunction) => {
+    return res.status(200).send(res.locals.metrics);
+  },
+);
 
 export default AWSRouter;
