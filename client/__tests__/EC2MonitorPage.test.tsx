6a2b5b187cb1080f14a02f552432a8d847b405ef
Christie Laferriere <christie.laferriere@gmail.com>
2024-06-04 18:47:06 -0400

    added a dark mode and updated the nav bar test

--- client/__tests__/EC2MonitorPage.test.tsx
+++ client/__tests__/EC2MonitorPage.test.tsx
@@ -1,3 +1,23 @@
+// import React from 'react';
+// import { render, screen } from '@testing-library/react';
+// import { MemoryRouter } from 'react-router-dom';
+// import Navbar from '../src/component/Navbar';
+
+// describe('NavBar', () => {
+//   it('renders navigation links and logo', () => {
+//     render(
+//       <MemoryRouter>
+//         <Navbar />
+//       </MemoryRouter>,
+//     );
+
+//     // Check for logo and links
+//     expect(screen.getByAltText('Logo')).toBeInTheDocument();
+//     expect(screen.getByText('Home')).toBeInTheDocument();
+//     expect(screen.getByText('EC2 Monitor')).toBeInTheDocument();
+//   });
+// });
+
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
@@ -5,9 +25,11 @@ import Navbar from '../src/component/Navbar';
 
 describe('NavBar', () => {
   it('renders navigation links and logo', () => {
+    const mockToggleDarkMode = jest.fn();
+
     render(
       <MemoryRouter>
-        <Navbar />
+        <Navbar isDarkMode={false} toggleDarkMode={mockToggleDarkMode} />
       </MemoryRouter>,
     );
 
@@ -15,5 +37,6 @@ describe('NavBar', () => {
     expect(screen.getByAltText('Logo')).toBeInTheDocument();
     expect(screen.getByText('Home')).toBeInTheDocument();
     expect(screen.getByText('EC2 Monitor')).toBeInTheDocument();
+    expect(screen.getByText('Dark Mode')).toBeInTheDocument();
   });
 });
