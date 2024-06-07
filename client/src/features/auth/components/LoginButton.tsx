5c46498d4a7fc0f5a0c13aa25b0f5bf0c616640a
b-the-coder <binhe1989@gmail.com>
2024-06-06 20:15:04 -0400

    update credential i nfos and loginbutton link, fixed NavBar.
    Co-authored-by: Nikolaa92 <Nikolaa92@users.noreply.github.com>
    Co-authored-by: ChristieLaf <ChristieLaf@users.noreply.github.com>
    Co-authored-by: abelr20 <abelr20@users.noreply.github.com>
    Co-authored-by: TrippMurphy <TrippMurphy@users.noreply.github.com>

--- client/src/features/auth/components/LoginButton.tsx
+++ client/src/features/auth/components/LoginButton.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const LoginButton: React.FC = () => {
   const navigate = () => {
     window.location.href =
-      'https://skyscraper.auth.us-east-2.amazoncognito.com/login?client_id=3je02pgra9uoqpjb46ckvsba82&response_type=token&scope=email+openid+phone&redirect_uri=http%3A%2F%2Flocalhost%3A8080%2Fdashboard';
+      'https://skyscraperwerock.auth.us-east-2.amazoncognito.com/oauth2/authorize?client_id=6hjtfh1ddmn4afj4c29ddijj32&response_type=token&scope=email+openid+phone+profile&redirect_uri=http%3A%2F%2Flocalhost%3A8080%2Fdashboard';
   };
 
   return (
