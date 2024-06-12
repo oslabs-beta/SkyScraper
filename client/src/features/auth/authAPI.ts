74eca8e713c013444e094816c5891b87d657fdd5
Abel Ratanaphan <abelrat@yahoo.com>
2024-06-12 05:50:56 -0400

    Add https support for deploy

--- client/src/features/auth/authAPI.ts
+++ client/src/features/auth/authAPI.ts
@@ -5,7 +5,7 @@ import { EC2Instance, EC2Stats } from '../../app/types';
 export const authApi = createApi({
   reducerPath: 'authApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://skyscraper-api.com/api/',
+    baseUrl: 'https://skyscraper-api.com/api/',
     prepareHeaders: (headers, { getState }) => {
       const state = getState() as RootState;
       const { access_token, id_token } = state.rootReducer.auth.tokens;
