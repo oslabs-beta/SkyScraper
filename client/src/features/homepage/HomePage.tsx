a576fbfe5def1654b45550d27668f61a21024bda
Abel Ratanaphan <abelrat@yahoo.com>
2024-08-14 17:08:43 -0400

    Test if ECR is fixed

--- client/src/features/homepage/HomePage.tsx
+++ client/src/features/homepage/HomePage.tsx
@@ -29,7 +29,7 @@ const HomePage: React.FC = () => {
     <div>
       <div className='login-button-container'>
         <div className='HpHeader'>
-          <h1> Welcome to SkyScraper</h1>
+          <h1> Welcome to SkyScraper </h1>
           <h2> Github Actions CI/CD </h2>
         </div>
         <div className='LoginButtonWrapper'>
